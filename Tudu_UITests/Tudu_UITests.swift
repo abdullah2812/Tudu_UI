6cd42da0afa7ded6380cc4c40c6093ebdbf98425
Abdullah <iriandaabdullah546@gmail.com>
2022-02-19 14:05:17 +0700

    Initial Commit

---
+++ Tudu_UITests/Tudu_UITests.swift
@@ -0,0 +1,33 @@
+//
+//  Tudu_UITests.swift
+//  Tudu_UITests
+//
+//  Created by Irianda on 19/02/22.
+//
+
+import XCTest
+@testable import Tudu_UI
+
+class Tudu_UITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
